9513f87b7ca98e5966ff6c04877acc072f437a6e
2204922-univesp <2204922@aluno.univesp.br>
2024-03-19 21:18:06 -0300

    feat: adicionado subtitulo do projeto

--- example/views.py
+++ example/views.py
@@ -9,6 +9,7 @@ def index(request):
     <html>
         <body>
             <h1>Hello from Vercel!</h1>
+            <h2>Aplicativo para o projeto integrador da univesp, ano 2024, primeiro semestre, grupo 22</h2>
             <p>The current time is { now }.</p>
         </body>
     </html>
