50f1218f1fb85e0916e3df302c4d71885c1c545c
Lincoln-Hernandes2024-univesp <164270461+Lincoln-Hernandes2024-univesp@users.noreply.github.com>
2024-04-29 23:41:09 -0300

    Update settings.py; arrumando um erro de sintaxe (corrigido)

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -136,7 +136,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'projeto', 'static'),
     
-],
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
