6e86518d8ab806059d2c4445313cdc2565f36688
2204922-univesp <2204922@aluno.univesp.br>
2024-05-01 18:58:45 -0300

    login obrigatório para gestão de clientes

--- cliente/urls.py
+++ cliente/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from cliente.views import cadastroClienteView, listaClienteView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('newCliente/', cadastroClienteView, name='newcliente'),
-    path('clientes/', listaClienteView, name='clientes')
+    path('newCliente/', login_required(cadastroClienteView), name='newcliente'),
+    path('clientes/', login_required(listaClienteView), name='clientes'),
+    path('login/', LoginView.as_view(template_name='admin/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
 
--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -20,6 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 from dotenv import load_dotenv
 load_dotenv()
 
+# Login defs
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
