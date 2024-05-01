8056476fd3fe80bb65ef0c8c9b7ac472121b12f6
2204922-univesp <2204922@aluno.univesp.br>
2024-05-01 19:05:13 -0300

    data de pagamento da despesa

--- despesa/models.py
+++ despesa/models.py
@@ -16,7 +16,7 @@ class Despesa(models.Model):
     tipoDespesa = models.ForeignKey(TipoDespesa, on_delete=models.PROTECT)
     descricao = models.TextField(null=False, blank=False)
     valor = models.DecimalField(max_digits=12, decimal_places=2, null=False, blank=False)
-    
+    dataPagamento = models.DateField(null=False, blank=False)
     class Meta:
         
         verbose_name_plural = 'Despesas'
