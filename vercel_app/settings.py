6e51f226e8551ae1f2c049bc102d5924bed4b58a
2204922-univesp <2204922@aluno.univesp.br>
2024-05-01 18:57:11 -0300

    carregar variaveis de ambiente do .env

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -16,6 +16,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Env variables
+from dotenv import load_dotenv
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
