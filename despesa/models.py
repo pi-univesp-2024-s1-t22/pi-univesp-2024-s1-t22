db179bdaa10293874967ca68e3d4dd17d3f263bc
2204922-univesp <2204922@aluno.univesp.br>
2024-05-06 21:11:02 -0300

    cadastro de listagem de recebimentos

--- despesa/models.py
+++ despesa/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-
-
+from django.core.validators import MinValueValidator
 
 class TipoDespesa(models.Model):
     
@@ -15,7 +14,7 @@ class Despesa(models.Model):
     
     tipoDespesa = models.ForeignKey(TipoDespesa, on_delete=models.PROTECT)
     descricao = models.TextField(null=False, blank=False)
-    valor = models.DecimalField(max_digits=12, decimal_places=2, null=False, blank=False)
+    valor = models.DecimalField(max_digits=12, decimal_places=2, null=False, blank=False, validators=[MinValueValidator(0.01)])
     dataPagamento = models.DateField(null=False, blank=False)
     class Meta:
         
--- recebimento/views.py
+++ recebimento/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpRequest
+
+from django.contrib import messages
+from django.urls import reverse
+
+from recebimento.models import Recebimentos
+from recebimento.forms import RecebimentoForm
 
 # Create your views here.
+
+def cadastroRecebimentoView(req: HttpRequest):
+    
+    if req.method == 'POST':
+        
+        form = RecebimentoForm(req.POST)
+        
+        if form.is_valid():
+            
+            try:
+                form.save()
+                messages.success(req, 'Recebimento criado com sucesso')
+                return redirect(reverse('recebimentos'))
+            
+            except Exception as e:
+                messages.error(req, e)
+        else:
+            print(form.errors)
+            for e in form.errors:
+                print(e)
+            messages.error(req, e)        
+        
+    
+    
+    context = { 'form': RecebimentoForm() }
+    
+    return render(req, 'cadastroRecebimento.html', context)
+
+
+def listaRecebimentoView(req: HttpRequest):
+    
+    recebimentos = Recebimentos.objects.all().reverse()
+    
+    context = { 'recebimentos': recebimentos}
+    
+    return render(req, 'listaRecebimento.html', context)
